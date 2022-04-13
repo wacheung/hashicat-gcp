e0b8cf17d8c312e30d39ac37004deb6585c5c586
wacheung <wacheung1@gmail.com>
2022-04-13 19:10:03 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+module "network" {
+  source  = "app.terraform.io/hashicat-gcp-test/network/google"
+  version = "3.4.0"
+  network_name = w-network
+  project_id = var.project
+  subnets = [
+  {
+    subnet_name   = "w-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
